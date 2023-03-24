e87bb66891ef068e583739f9890c6f6a7b167828
André Martins <128826621+andmartins7@users.noreply.github.com>
2023-03-24 13:51:54 -0300

    Tratamento de erro na chave da API

--- PromptBot/PromptBot.py
+++ PromptBot/PromptBot.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request
 import openai
+import os
 
 app = Flask(__name__)
 
 # Configure OpenAI API key
-openai.api_key = "OpenAI_API_key"
+if "OPENAI_API_KEY" in os.environ:
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+else:
+    print("Erro: variável de ambiente OPENAI_API_KEY não definida.")
+    exit()
 
 # Define home page
 @app.route("/")
@@ -14,16 +19,20 @@ def home():
 # Define endpoint for generating prompts
 @app.route("/generate_prompts", methods=["POST"])
 def generate_prompts():
-    prompt = request.form.get("prompt")
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    return render_template("result.html", prompt=prompt, result=response.choices[0].text)
+    try:
+        prompt = request.form.get("prompt")
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        return render_template("result.html", prompt=prompt, result=response.choices[0].text)
+    except Exception as e:
+        print("Error:", e)
+        return render_template("error.html", error=str(e))
 
 if __name__ == "__main__":
     app.run(debug=True)
